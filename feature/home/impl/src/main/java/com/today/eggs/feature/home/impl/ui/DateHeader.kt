55fc4c04c0aac70aa365200922f9816ba8286178
ggujangi <asmwj3@naver.com>
2026-09-20 21:40:58 +0900

    #4 DateHeader Preview 생성

--- feature/home/impl/src/main/java/com/today/eggs/feature/home/impl/ui/DateHeader.kt
+++ feature/home/impl/src/main/java/com/today/eggs/feature/home/impl/ui/DateHeader.kt
@@ -1,11 +1,13 @@
 package com.today.eggs.feature.home.impl.ui
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.datetime.LocalDate
 
 @Composable
@@ -22,5 +24,18 @@ fun DateHeader(
         color = MaterialTheme.colorScheme.primary,
         textAlign = TextAlign.Center,
         maxLines = 1,
+    )
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Date Header"
+)
+fun DateHeaderPreview() {
+    DateHeader(
+        date = LocalDate(2023, 12, 1),
+        modifier = Modifier.fillMaxWidth()
     )
 }
